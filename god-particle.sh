451b16b4abd661b1170607de8e95095d612237a9
omega808 <donjohnson1149@gmail.com>
2020-09-20 14:36:53 -0400

    Cleaned up the actual script, made more user freindly

--- god-particle.sh
+++ god-particle.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 #Author:NewWavex86
 #Startup script for the raspberry pi, modified Git vewrsion for users not using my local configurations
- 
-N=0 #Another container for loop
+
+#source $HOME/snippets/color #Cant do for git
+
 VAR=0 #Container for while loop
 
-#Add usb variables to backup
-read -p "Do you want to do backups to a usb [Y\n]" USB
-	if [[ $USB =~ [y-Y] ]];
-	then
-		sed -i ' 6c\ USB_BACKUP\=\"Y\" ' backup.sh
-		
-		while [ $N -lt 1 ];
-		do
-			read -p "Enter the full path of your usb: " USB_PATH
-
-			#check if path is entered correctly
-			if ! [[  $USB_PATH =~ ^/ ||  $USB_PATH =~ $/ ]];
-			then
-				echo "make sure to enter full path"
-			else #User entered full path, exit loop and write path to script
-				let N++ 
-				sed -i " 5c\ USB_PATH\=\"$USB_PATH\" "
-			fi
-		done		
-	else 
-		#Write to backup to not do usb backup
-		sed -i ' 6c\USB_PATH\=\"N\" ' backup.sh
-	fi
-
-#For git, create a bin directory and move the backup scripts to it
-mkdir /home/pi/bin
-#Make the backup file excutable
-sudo chmod 755 backup.sh
-mv backup.sh /home/pi/bin/backup
+#For git, create a bin directory and move the backup script to it
+#mkdir /home/pi/bin
+#mv backup.sh /home/pi/bin/backup
 
 
 #Create array of packages to check
-PACKAGES=("tmux" "htop" "nmap" "neofetch" "vim" "figlet" "cmatrix" "terminology" "rename")
+PACKAGES=("tmux" "htop" "nmap" "neofetch" "vim" "docker.io" "figlet" "cmatrix" "terminology" "rename")
 ARRAYSIZE=${#PACKAGES[@]} #This variable is created because the length of array would decrease while
 			 #through the while loop
 
+package-install(){
+
+echo "Checking installed packages: "
+#Run through array, and remove already installed packages
+while [ $VAR -lt $ARRAYSIZE  ];
+do
+	echo "Checking ${PACKAGES[$VAR]}"
+	sleep 1
+						#Redirect Standard Error to prevent errors coming on user's screen
+	if [[ $(dpkg-query -s ${PACKAGES[$VAR]} 2> /dev/null ) ]];
+	then
+		echo "       Installed!"
+	else
+		echo "       Need to install"
+		unset ${PACKAGES[$VAR]} #Remove installed packages from array
+	fi
+	
+	let VAR++
+	
+done
+
+
+#Install the uninstalled
+echo "Installing the packages"
+sudo apt install ${PACKAGES[@]}
+
+}
+
+
 #Turned into a function so I can run this after VIM is installed
 vim-line-count(){
+	
 	#Add line count to VI
 	read -p "Would you like to add line count to Vim?[Y/n]" REPLY1
 		if [[ "$REPLY1" =~ ^[y-Y]$ ]];
@@ -50,39 +55,40 @@ vim-line-count(){
 			sudo sed -i ' 50i\set number ' /etc/vim/vimrc
 		fi
 }
-#Startup Scripts
+
+
+#Startup Scripts that run in the .bashrc file
 startup-scripts(){
 
- 	read -p "Would you like any scripts to be run automatically when you log in[Y/n]" RE
-        
+	read -p "Would you like any scripts to be run automatically when you log in[Y/n]" RE
 	if [[ "$RE" =~ [y-Y]+ ]];
-        then
-                #Setup counter for loop
-                N=1
-                #Loops allows user to input mutiple scripts
+	then
+		#Setup counter for loop
+		N=1
 		while [ $N -le 2 ];
-                do
-                        read -p "Enter script name: " SCRIPT
+		do       
+			read -p "Enter script name: " SCRIPT
+	       		
+ 			#Echo would suffice here, but I use sed for practice			
+			LENGTH=$( wc -l /home/pi/.bashrc | awk ' {print $1}' )
+			sudo sed -i "${LENGTH}i\ sudo bash $SCRIPT " /home/pi/.bashrc
+			
+			read -p "Would you like to add another script[Y/n]" REPLY
+			if [[ $REPLY =~ [y-Y]+ ]];
+			then
+				N=1
+			
+			else
+				N=2
 
-                        #Echo would suffice here, but I use sed for practice
-                        LENGTH=$( wc -l /home/pi/.bashrc | awk ' {print $1}' ) #Figure out last line of .bashrc
-                        sudo sed -i "${LENGTH}i\ sudo bash $SCRIPT " /home/pi/.bashrc
+			fi
+		done
+	fi		
 
-                        read -p "Would you like to add another script[Y/n]" REPLY
-                        if [[ $REPLY =~ [y-Y]+ ]];
-                        then
-                                N=1
-
-                        else
-                                N=2
-
-                        fi
-                done
-        fi
-
+				
 }
 
-
+add-path(){
 #check if ~/bin is added to env Path
 cat /etc/profile | grep ^PATH > /dev/null
 EXITSTATUS=$?
@@ -99,6 +105,9 @@ then
 fi
 
 
+}
+
+backup-job(){
 #Added for git users forking code
 if [ ! -f /home/pi/bin/backup ];
 then
@@ -116,28 +125,30 @@ else
 		fi
 fi
 
-echo "Checking installed packages: "
-#Run through array, and remove already installed packages
-while [ $VAR -lt $ARRAYSIZE  ];
-do
-	sleep 1
-	dpkg -l | grep -i ${PACKAGES[$VAR]} && unset PACKAGES[$VAR]
-	let VAR++
-	
-done
 
-#Install the uninstalled
-echo "Installing the packages"
-sudo apt install ${PACKAGES[@]}
+}
+
+#The actual program
+echo "The Unix wizard casts his spell.."
+sleep 1
+echo "It begins.."
+#Add bin folder to env path
+add-path
+
+#Make cron run the backup scripts
+backup-job
+
+#Install various packages
+package-install
 
 #Add line count to VIM after it is installed
 vim-line-count
 
-#Allow user-inputed scripts to run upon login
+#Make user input scripts run at startup
 startup-scripts
 
-echo  " Everything is good to go! "
-shutdown -r
+echo -e  "The Unix wizard disappears back into the ether"
+sudo reboot
 
 
 exit 0
